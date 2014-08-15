c4a3d2d136568d6e1b88ba6de0f2d4dbbeed5397
Mal Jones <mjones@forumone.com>
2014-08-14 20:41:27 -0400

    Layout in place! Holy smokes!

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,7 +1,27 @@
 angular.module('speakagentAAC.controllers', [])
 
 
+.controller('SettingsCtrl', function($scope, $ionicModal) {
+
+
+  // Create the Settings modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/settings.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  // Triggered in the login modal to close it
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  // Open the Settings modal
+  $scope.settings = function() {
+    $scope.modal.show();
+  }
+})
+
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
 
   // Form data for the login modal
   $scope.loginData = {};
@@ -13,15 +33,19 @@ angular.module('speakagentAAC.controllers', [])
     $scope.modal = modal;
   });
 
+
+
   // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
+  $scope.closeModal = function() {
     $scope.modal.hide();
-  },
+  };
 
   // Open the login modal
   $scope.login = function() {
     $scope.modal.show();
   };
+
+
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
