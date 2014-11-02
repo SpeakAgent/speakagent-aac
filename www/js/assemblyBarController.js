dfba884fea94110769929c20c5b9bcce618a1350
Baron Chandler <baron@venturecranial.com>
2014-11-01 22:25:21 -0400

    #1 - drag stops working on subsequent boards
        - Added timeout call to re-enable drag on each page after it loads.

--- www/js/assemblyBarController.js
+++ www/js/assemblyBarController.js
@@ -3,166 +3,168 @@ angular.module('speakagentAAC.controllers.AssemblyBar', ['ionic'])
 .controller('AssemblyBar', ['$scope', '$window', '$rootScope',
   'clearAssemblyBar', 'addTileToAssemblyBar', 'getAssemblyBarTiles',
   'assemblyBarTileCount', 'setClearOnAdd', 'moveTileInFrontOfIndex',
-  'deleteAssemblyBarTileAtIndex',
+  'deleteAssemblyBarTileAtIndex', '$timeout',
 
   function($scope, win, $rootScope, clearAssemblyBar, addTileToAssemblyBar,
     getAssemblyBarTiles, assemblyBarTileCount, setClearOnAdd,
-    moveTileInFrontOfIndex, deleteAssemblyBarTileAtIndex) {
+    moveTileInFrontOfIndex, deleteAssemblyBarTileAtIndex, $timeout) {
 
-  ionic.onGesture('dragstart', function(e) {
+    $scope.enableDrag = function() {
 
-    // find our element in the list, and make sure it's the tile and not
-    // something inside it.
-    //
-    $scope.draggedElement = angular.element(e.srcElement);
+      ionic.onGesture('dragstart', function(e) {
 
-    while (!$scope.draggedElement.hasClass('tile') && !$scope.draggedElement.hasClass('assembly-bar')) {
-      $scope.draggedElement = $scope.draggedElement.parent();
-    }
-
-    if ($scope.draggedElement.hasClass('tile')) {
-
-      // Mark it as being dragged
-      $scope.draggedElement.addClass('dragging');
-
-      // Record where the user touched it so we can track where it
-      // moves to.
-      //
-      $scope.dragStartX =  e.gesture.touches[0].pageX;
-
-      // Record which tile is actually moving
-      //
-      $scope.moveMeIndex = $scope.draggedElement.data('$scope').$index;
-
-      // console.log('Moving tile #'+$scope.moveMeIndex);
-      // var tiles = [];
-      // $scope.assemblyBarPhrase.forEach(function(element) {
-      //   tiles.push(element.name);
-      // });
-
-      // console.log('AssemblyBarPhrases are ', tiles);
-
-      // Update our display.
-      //
-      $scope.$apply();
-
-    } else {
-      $scope.draggedElement = null;
-      $scope.dragStartX = null;
-      $scope.moveMeIndex = null;
-    }
-  }, document.getElementById('assembly-bar'));
-
-  // Handle the actual DRAG
-  //
-  ionic.onGesture('drag', function(e) {
-
-    if ($scope.nowMoving !== null) {
-
-      // How far have we moved, horizontally speaking?
-      var x = e.gesture.touches[0].pageX - $scope.dragStartX;
-
-      // Find the tile that we're moving OVER
-
-      var tile = $scope.findTileXY(e.gesture.touches[0].pageX, 0);
-
-      if (tile) {
-
-        // Find out which tile we will put the moving tile in front of.
+        // find our element in the list, and make sure it's the tile and not
+        // something inside it.
         //
-        var insertBeforeIndex = tile.data('$scope').$index;
+        $scope.draggedElement = angular.element(e.srcElement);
 
-        // Find and yoink the tile we are moving (the one under our finger)
-        // from the list.
-
-        moveTileInFrontOfIndex($scope.moveMeIndex, insertBeforeIndex);
-        // var moved = $scope.assemblyBarPhrase.splice($scope.moveMeIndex, 1);
-
-
-        // // console.log('tile ', tile);
-        // // console.log('insertBeforeIndex ' + insertBeforeIndex);
-        // // console.log('moved ', moved);
-
-        // // Move the tile into position.
-        // //
-        // $scope.assemblyBarPhrase.splice(insertBeforeIndex, 0, moved[0]);
-
-        // Update the index to reflect that we've moved it.
-        //
-        $scope.moveMeIndex = insertBeforeIndex;
-
-        // var tiles = [];
-        // $scope.assemblyBarPhrase.forEach(function(element) {
-        //   tiles.push(element.name);
-        // });
-
-        // console.log('AssemblyBarPhrases are ', tiles);
-
-        // Update the phrase.
-        //
-        // $rootScope.assemblyBarPhrase = $scope.assemblyBarPhrase;
-        $scope.$apply();
+        while (!$scope.draggedElement.hasClass('tile') && !$scope.draggedElement.hasClass('assembly-bar')) {
+          $scope.draggedElement = $scope.draggedElement.parent();
         }
-    }
 
-  }, document.getElementById('assembly-bar'));
+        if ($scope.draggedElement.hasClass('tile')) {
 
+          // Mark it as being dragged
+          $scope.draggedElement.addClass('dragging');
 
-  // We're done dragging.
-  ionic.onGesture('dragend', function(e) {
-    if ($scope.draggedElement) {
-      $scope.draggedElement.css(ionic.CSS.TRANSFORM, '');
-      angular.element($scope.draggedElement).removeClass('dragging');
-    }
-    $scope.draggedElement = null;
-    $scope.dragStartX = null;
-    $scope.moveMeIndex = null;
-
-    // They changed the phrase, so let's not clear it
-    //
-    setClearOnAdd(false);
-
-  }, document.getElementById('assembly-bar'));
-
-  // Look in our list of tiles and find the tile that is under the x, y
-  // coordinates.
-  //
-  $scope.findTileXY = function(x, y) {
-
-      /* this is old school; we can do it better i am sure */
-      console.log('in findtilexy');
-      var bar = angular.element(document.getElementById('assembly-bar'));
-      var kids = angular.element(bar.children()[0]).children();
-
-      for (var index = 0; index < kids.length; index++) {
-        var rect = kids[index].getBoundingClientRect();
-        if ((x > rect.left) && (x < rect.right)) {
-          var child = angular.element(kids[index]);
-          // Of course, we want to ignore the tile that is currently
-          // being dragged around.
+          // Record where the user touched it so we can track where it
+          // moves to.
           //
-          if (!child.hasClass('dragging')) {
-            return child;
+          $scope.dragStartX =  e.gesture.touches[0].pageX;
+
+          // Record which tile is actually moving
+          //
+          $scope.moveMeIndex = $scope.draggedElement.data('$scope').$index;
+
+          // console.log('Moving tile #'+$scope.moveMeIndex);
+          // var tiles = [];
+          // $scope.assemblyBarPhrase.forEach(function(element) {
+          //   tiles.push(element.name);
+          // });
+
+          // console.log('AssemblyBarPhrases are ', tiles);
+
+          // Update our display.
+          //
+          $scope.$apply();
+
+        } else {
+          $scope.draggedElement = null;
+          $scope.dragStartX = null;
+          $scope.moveMeIndex = null;
+        }
+      }, document.getElementById('assembly-bar'));
+
+      // Handle the actual DRAG
+      //
+      ionic.onGesture('drag', function(e) {
+
+        if ($scope.nowMoving !== null) {
+
+          // How far have we moved, horizontally speaking?
+          var x = e.gesture.touches[0].pageX - $scope.dragStartX;
+
+          // Find the tile that we're moving OVER
+
+          var tile = $scope.findTileXY(e.gesture.touches[0].pageX, 0);
+
+          if (tile) {
+
+            // Find out which tile we will put the moving tile in front of.
+            //
+            var insertBeforeIndex = tile.data('$scope').$index;
+
+            // Find and yoink the tile we are moving (the one under our finger)
+            // from the list.
+
+            moveTileInFrontOfIndex($scope.moveMeIndex, insertBeforeIndex);
+            // var moved = $scope.assemblyBarPhrase.splice($scope.moveMeIndex, 1);
+
+
+            // // console.log('tile ', tile);
+            // // console.log('insertBeforeIndex ' + insertBeforeIndex);
+            // // console.log('moved ', moved);
+
+            // // Move the tile into position.
+            // //
+            // $scope.assemblyBarPhrase.splice(insertBeforeIndex, 0, moved[0]);
+
+            // Update the index to reflect that we've moved it.
+            //
+            $scope.moveMeIndex = insertBeforeIndex;
+
+            // var tiles = [];
+            // $scope.assemblyBarPhrase.forEach(function(element) {
+            //   tiles.push(element.name);
+            // });
+
+            // console.log('AssemblyBarPhrases are ', tiles);
+
+            // Update the phrase.
+            //
+            // $rootScope.assemblyBarPhrase = $scope.assemblyBarPhrase;
+            $scope.$apply();
+            }
+        }
+
+      }, document.getElementById('assembly-bar'));
+
+      // We're done dragging.
+      ionic.onGesture('dragend', function(e) {
+        if ($scope.draggedElement) {
+          $scope.draggedElement.css(ionic.CSS.TRANSFORM, '');
+          angular.element($scope.draggedElement).removeClass('dragging');
+        }
+        $scope.draggedElement = null;
+        $scope.dragStartX = null;
+        $scope.moveMeIndex = null;
+
+        // They changed the phrase, so let's not clear it
+        //
+        setClearOnAdd(false);
+
+      }, document.getElementById('assembly-bar'));
+    };
+
+    $timeout($scope.enableDrag);
+
+    // Look in our list of tiles and find the tile that is under the x, y
+    // coordinates.
+    //
+    $scope.findTileXY = function(x, y) {
+        var bar = angular.element(document.getElementById('assembly-bar'));
+        var kids = angular.element(bar.children()[0]).children();
+
+        for (var index = 0; index < kids.length; index++) {
+          var rect = kids[index].getBoundingClientRect();
+          if ((x > rect.left) && (x < rect.right)) {
+            var child = angular.element(kids[index]);
+            // Of course, we want to ignore the tile that is currently
+            // being dragged around.
+            //
+            if (!child.hasClass('dragging')) {
+              return child;
+            }
           }
         }
+        return null;
+    };
+
+
+    $scope.barTileClicked = function(index, obj) {
+      // console.log('bar tile clicked: ',  obj);
+
+      newTiles = deleteAssemblyBarTileAtIndex(index);
+
+      if($rootScope.AnalyticsAvailable) {
+        analytics.trackEvent('Boards', 'TileRemove', obj.phrase);
       }
-      return null;
-  };
+    };
 
-  $scope.barTileClicked = function(index, obj) {
-    // console.log('bar tile clicked: ',  obj);
-
-    newTiles = deleteAssemblyBarTileAtIndex(index);
-
-    if($rootScope.AnalyticsAvailable) {
-      analytics.trackEvent('Boards', 'TileRemove', obj.phrase);
-    }
-  };
-
-  $scope.barContents = function() {
-    return getAssemblyBarTiles();
-  };
-}])
+    $scope.barContents = function() {
+      return getAssemblyBarTiles();
+    };
+  }])
 
 .factory('addTileToAssemblyBar', ['$window', 'setClearOnAdd',
   function(win, setClearOnAdd) {
